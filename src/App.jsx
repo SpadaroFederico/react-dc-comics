354e8f880277492284972b167489245f4e243358
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-14 16:53:08 +0100

    [DONE] header + Main tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from "./components/Header.jsx";
 // import Footer from "./components/Footer.jsx";
-// import Main from "./components/Main.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import logo from '../assets/img/dc-logo.png';
-const Header = () => {
-    return (
-      <header>
-        <img src={logo} alt="Logo"/>
-      </header>
-    );
-  };
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <a href="#"><img src={logo} alt="Logo" /></a>
+        <nav>
+          <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIE</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
   
