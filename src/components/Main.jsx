622933a4956490f9ba72ede159c562906c4de905
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-15 15:44:05 +0100

    [ADD] main links part

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,41 @@
+import comics from '../assets/img/buy-comics-digital-comics.png';
+import merchandise from '../assets/img/buy-comics-merchandise.png';
+import locater from '../assets/img/buy-comics-shop-locator.png';
+import subscriptions from '../assets/img/buy-comics-subscriptions.png';
+import visa from '../assets/img/buy-dc-power-visa.svg';
+
+
 export default function Main (){
     return (
-        <div className="container-black">
-            <h1>&#8594;CONTENT GOES HERE&#8592;</h1>
-        </div>
+        <main>
+            <div className="container-black">
+                <h2>&#8594; Content goes here &#8592;</h2>
+            </div>
+
+            <div className="container-blue">
+                <a href="#"><img src={comics} alt="comics" />
+                    <span>DIGITAL COMICS</span>
+                </a>
+                <a href="#"><img src={merchandise} alt="merchandise" />
+                    <span>DC MERCHANDISE</span>
+                </a>
+                
+                <a href="#"><img src={subscriptions} alt="subscriptions" />
+                    <span>SUBSCRIPTIONS</span>
+                </a>
+                
+                <a href="#"><img src={locater} alt="locater" />
+                    <span>COMICS STOP LOCATOR</span>
+                </a>
+          
+                <a href="#"><img src={visa} alt="visa" />
+                    <span>DC POWER VISA</span>
+                </a>
+            </div>
+
+            <div className="container-jumbotron">
+                
+            </div>
+        </main>
     );
 }
